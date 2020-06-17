e5bc2a2c525df61b23d59f57ba4e937d6e2b2bca
anandhu-bhaskar <anandhu.da3@iiitmk.ac.in>
2020-06-17 13:20:08 +0530

    WAQI Ist commit

---
+++ dwd.sh
@@ -0,0 +1,9 @@
+# !curl --compressed -o waqi-covid-2020.csv   https://aqicn.org/data-platform/covid19/report/13012-63037224/2020
+# !curl --compressed -o waqi-covid-2019Q1.csv https://aqicn.org/data-platform/covid19/report/13012-63037224/2019Q1
+# !curl --compressed -o waqi-covid-2019Q2.csv https://aqicn.org/data-platform/covid19/report/13012-63037224/2019Q2
+# !curl --compressed -o waqi-covid-2019Q3.csv https://aqicn.org/data-platform/covid19/report/13012-63037224/2019Q3
+# !curl --compressed -o waqi-covid-2019Q4.csv https://aqicn.org/data-platform/covid19/report/13012-63037224/2019Q4
+# !curl --compressed -o waqi-covid-2018H1.csv https://aqicn.org/data-platform/covid19/report/13012-63037224/2018H1
+# !curl --compressed -o waqi-covid-2017H1.csv https://aqicn.org/data-platform/covid19/report/13012-63037224/2017H1
+# !curl --compressed -o waqi-covid-2016H1.csv https://aqicn.org/data-platform/covid19/report/13012-63037224/2016H1
+# !curl --compressed -o waqi-covid-2015H1.csv https://aqicn.org/data-platform/covid19/report/13012-63037224/2015H1
